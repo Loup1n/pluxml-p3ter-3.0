93b7ac2ff768710a587d85322a4a6dff5cf837e3
P3ter <contact@p3ter.fr>
2018-04-25 16:16:40 +0200

    Ajout du favicon.ico

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
 	<?php $plxShow->meta('keywords') ?>
 	<?php $plxShow->meta('author') ?>
 	<link rel="icon" href="<?php $plxShow->template(); ?>/img/favicon.png" />
+	<link rel="icon" href="<?php $plxShow->template(); ?>/img/favicon.ico" />
 	<link rel="stylesheet" href="<?php $plxShow->template(); ?>/css/plucss.css" media="screen"/>
 	<link rel="stylesheet" href="<?php $plxShow->template(); ?>/css/theme.css" media="screen"/>
 	<?php $plxShow->templateCss() ?>
